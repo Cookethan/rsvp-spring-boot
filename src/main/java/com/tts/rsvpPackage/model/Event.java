b678e174626b96417599b44bdd9e394bc5fadea6
cookethan <ethancook48@gmail.com>
2020-04-24 08:58:47 -0500

    added user/event relationship

--- src/main/java/com/tts/rsvpPackage/model/Event.java
+++ src/main/java/com/tts/rsvpPackage/model/Event.java
@@ -1,22 +1,34 @@
 package com.tts.rsvpPackage.model;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property="id")
 @Entity
 public class Event {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="event_id")
 	private Long id;
+
 	private String name;
 	private String location;
 	private Float cost;
 	private Date date;
-	
+		
 	public Event() {}
 
 	public Event(String name, String location, Float cost, Date date) {
@@ -26,6 +38,14 @@ public class Event {
 		this.date = date;
 	}
 
+	public Set<User> getUsers() {
+		return users;
+	}
+	
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -61,11 +81,25 @@ public class Event {
 	public Long getId() {
 		return id;
 	}
+	
+	public void addUser(User user) {
+		this.users.add(user);
+		user.getEvents().add(this);
+	}
+	
+	public void removeUser(User user) {
+		this.users.remove(user);
+		user.getEvents().remove(this);
+	}
 
 	@Override
 	public String toString() {
 		return "Event [name=" + name + ", location=" + location + ", cost=" + cost + ", date=" + date + "]";
 	}
+	
+	@ManyToMany(mappedBy="events")
+	private Set<User> users = new HashSet<>();
+	
 	
 	
 }
--- src/main/java/com/tts/rsvpPackage/model/User.java
+++ src/main/java/com/tts/rsvpPackage/model/User.java
@@ -1,14 +1,29 @@
 package com.tts.rsvpPackage.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property="id")
 @Entity
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="user_id")
 	private Long id;
 	private String name;
 	private String email;
@@ -18,6 +33,14 @@ public class User {
 	public User(String name, String email) {
 		this.name = name;
 		this.email = email;
+	}
+
+	public Set<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(Set<Event> events) {
+		this.events = events;
 	}
 
 	public String getName() {
@@ -35,6 +58,16 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public void addEvent(Event event) {
+		this.events.add(event);
+		event.getUsers().add(this);
+	}
+	
+	public void removeEvent(Event event) {
+		this.events.remove(event);
+		event.getUsers().remove(this);
+	}
 
 	public Long getId() {
 		return id;
@@ -45,6 +78,9 @@ public class User {
 		return "User [name=" + name + ", email=" + email + "]";
 	}
 	
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinTable(name="user_event", joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns = @JoinColumn(name="event_id"))
+	private Set<Event> events = new HashSet<>();
 	
 	
 }
